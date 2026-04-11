6000b9151c1c413a0666a4afb7f187d573c25b2c
Nadav <nadav.zaro@gmail.com>
2026-04-11 20:01:30 +0300

    sync to phone

--- test.js
+++ test.js
@@ -5,7 +5,7 @@ fetch('http://localhost:3000/ask-jarvis', {
         'Content-Type': 'application/json',
         'X-API-Key': process.env.APP_SECRET
     },
-    body: JSON.stringify({ command: 'תראה לי את רשימת המשימות שלי' })
+    body: JSON.stringify({ command: 'איפה אני נמצא???' })
 })
 .then(res => res.json())
 .then(data => console.log('Jarvis replied:', data.answer));
